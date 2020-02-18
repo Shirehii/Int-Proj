38f1bcfee7d98edec6f31cecc9c7759eee586b2e
Void <daigoukaruna@gmail.com>
2020-02-18 15:30:40 +0000

    basic animator implemented

---
+++ DecoSteam/Assets/Scripts/Animations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public PlayerController pc;
+    private Animator animator;
+
+    private void Start()
+    {
+        //Get Components
+        pc = gameObject.GetComponent<PlayerController>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.Play("Idle");
+    }
+}
--- DecoSteam/Assets/Scripts/GameManager.cs
+++ DecoSteam/Assets/Scripts/GameManager.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    //so this script is supposed to be used for global variables but i dont know how those work fjgkfdghj
 }
