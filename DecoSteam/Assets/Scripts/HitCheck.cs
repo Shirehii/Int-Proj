dec9ea01da63c8032fc01e00129bb0fced116c79
Kudev <YKUDEV200@caledonian.ac.uk>
2020-03-04 14:07:38 +0000

    meh

--- DecoSteam/Assets/Scripts/HitCheck.cs
+++ DecoSteam/Assets/Scripts/HitCheck.cs
@@ -6,19 +6,29 @@ public class HitCheck : MonoBehaviour
 {
     public PlayerCombat comb;
     public Movement mvmnt;
+	public PlayerController pc;
     public Rigidbody2D colly;
     public float pow = 2500f;
+	public gameObject player;
 
+	public void Awake()
+	{
+		pc = otherGameObject.GetComponent<"PlayerController">();
+	}
 
-    public void OnTriggerEnter2D(Collider2D col) //GroundCheck
+    public void OnTriggerEnter2D(Collider2D col) 
     {
-        Debug.Log("ATTACK");
-
-        
-
-        colly = col.gameObject.GetComponent<Rigidbody2D>();
-        colly.AddForce(new Vector2(pow, 0));
-
-        gameObject.SetActive(false);
+		if(pc.direction == 1)
+		{
+			colly = col.gameObject.GetComponent<Rigidbody2D>();
+			colly.AddForce(new Vector2(pow, 0));
+			gameObject.SetActive(false);
+		}	
+        if(pc.direction == -1)
+		{
+			colly = col.gameObject.GetComponent<Rigidbody2D>();
+			colly.AddForce(new Vector2(-pow, 0));
+			gameObject.SetActive(false);
+		}
     }
 }
