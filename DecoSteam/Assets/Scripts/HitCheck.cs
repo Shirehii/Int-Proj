2d1ded3cfb1f4dd8e4401d40e47dbdd54433b279
Shirehii <daigoukaruna@gmail.com>
2020-04-04 18:36:47 +0300

    shield bug fixed

--- DecoSteam/Assets/Scripts/HitCheck.cs
+++ DecoSteam/Assets/Scripts/HitCheck.cs
@@ -5,31 +5,42 @@ using UnityEngine;
 public class HitCheck : MonoBehaviour
 {
 	public PlayerController pc;
+    public PlayerCombat comb;
     public Rigidbody2D colly;
-    public int pow = 5000;
-    public bool enemyShielded; //for shield mechanic
+    public int pow = 500;
+    public bool enemyShielded; //for shield powerup
 
 	public void Awake()
 	{
         if (gameObject.tag == "Attp1")
         {
             pc = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerController>();
+            comb = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerCombat>();
         }
         if (gameObject.tag == "Attp2")
         {
             pc = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerController>();
+            comb = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerCombat>();
         }
 	}
 
-    public void OnTriggerEnter2D(Collider2D trigger) 
+    public void OnTriggerEnter2D(Collider2D trigger)
     {
         if (trigger.gameObject.layer == 8)
         {
             enemyShielded = trigger.gameObject.GetComponent<PlayerCombat>().shielded;
             if (!enemyShielded)
             {
-                colly = trigger.gameObject.GetComponent<Rigidbody2D>();
+                if (gameObject.tag == "Attp1") //damage
+                {
+                    comb.TakeDamageOnPlayer2(20f);
+                }
+                if (gameObject.tag == "Attp2")
+                {
+                    comb.TakeDamageOnPlayer1(20f);
+                }
 
+                colly = trigger.gameObject.GetComponent<Rigidbody2D>(); //knockback
                 if (pc.lastDirectionPressed == 1)
                 {
                     colly.AddForce(new Vector2(pow, 0));
@@ -40,6 +51,11 @@ public class HitCheck : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void LateUpdate()
+    {
+        gameObject.SetActive(false);
     }
 
     //F
--- DecoSteam/Assets/Scripts/PlayerCombat.cs
+++ DecoSteam/Assets/Scripts/PlayerCombat.cs
@@ -10,8 +10,7 @@ public class PlayerCombat : MonoBehaviour
     public float Player2HPMax = 100;
     public float Player2HPCurrent;
 
-    public GameObject attp1;
-    public GameObject attp2;
+    public GameObject attp;
 
     public PlayerController pc;
     public HitCheck hc;
@@ -23,16 +22,16 @@ public class PlayerCombat : MonoBehaviour
     public void Awake()
     {
         pc = gameObject.GetComponent<PlayerController>();
-
-        attp1 = GameObject.FindGameObjectWithTag("Attp1");
+        
         if (gameObject.tag == "Player1")
         {
-            hc = attp1.GetComponent<HitCheck>();
+            attp = GameObject.FindGameObjectWithTag("Attp1");
+            hc = attp.GetComponent<HitCheck>();
         }
-        attp2 = GameObject.FindGameObjectWithTag("Attp2");
         if (gameObject.tag == "Player2")
         {
-            hc = attp2.GetComponent<HitCheck>();
+            attp = GameObject.FindGameObjectWithTag("Attp2");
+            hc = attp.GetComponent<HitCheck>();
         }
     }
 
@@ -41,31 +40,13 @@ public class PlayerCombat : MonoBehaviour
         Player1HPCurrent = Player1HPMax;
         Player2HPCurrent = Player2HPMax;
 
-        attp1.SetActive(false);
-        attp2.SetActive(false);
+        attp.SetActive(false);
     }
 
 
     public void Attackplr()
     {
-        if (gameObject.tag == "Player1")
-        {
-            attp1.SetActive(true);
-            if (hc.enemyShielded)
-            {
-                TakeDamageOnPlayer2(20f);
-            }
-            attp1.SetActive(false);
-        }
-        if (gameObject.tag == "Player2")
-        {
-            attp2.SetActive(true);
-            if (hc.enemyShielded)
-            {
-                TakeDamageOnPlayer1(20f);
-            }
-            attp2.SetActive(false);
-        }
+        attp.SetActive(true);
         pc.attackPlr = false;
     }
 
